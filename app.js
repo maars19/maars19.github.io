f3c8401dabee43aec6e05d222a8f8c5a55857f78
maars19 <maariyajamil06@gmail.com>
2024-09-11 16:47:20 +0100

    initial commit

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 document.addEventListener("DOMContentLoaded" , () => {
-
-    const images = document.querySelectorAll("img");
-
-    for(const image of images){
-        console.log(image)
-        fetch("https://dog.ceo/api/breeds/image/random")
-        .then(response => response.json())
-        .then(data => {
-            image.src = data.message
-            image.width = 100;
-            image.height = 100;
-        })
-    }
-
-})
+    const translations
+    en
+   <script>
+   function changeLanguage(selectObj) {
+       var selectedLanguage = selectObj.value;
+       // Redirect to the selected language version of the website
+       window.location.href = selectedLanguage;
+   }
+</script>
+}
